5c24355a51f76f9e83ded4a4fbeb2cbefbcc408d
Bill Adams <vikingbill@gmail.com>
2021-03-25 16:22:58 -0400

    queue specs solved

--- 02-Queue/lib/queue.js
+++ 02-Queue/lib/queue.js
@@ -13,7 +13,7 @@
 // Constraints:
 // ------------
 //
-// Make sure the time and space complexity of each is equivalent to those 
+// Make sure the time and space complexity of each is equivalent to those
 // in the table provided in the Time and Space Complexity Analysis section
 // of your Stack reading!
 //
@@ -22,11 +22,50 @@
 // -----------
 
 class Node {
-
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
 }
 
 class Queue {
+  constructor(){
+    this.length = 0;
+    this.back = null;
+    this.front = null;
+  }
 
+  enqueue(value){
+    const oldNode = this.back;
+    const newNode = new Node(value);
+    this.length++;
+    if (this.front === null && this.back === null) {
+      this.front = newNode
+    }
+    this.back = newNode;
+    if (oldNode) oldNode.next = newNode;
+    return this.length;
+  }
+
+  dequeue(){
+    if (this.length === 0) return null;
+    const frontNode = this.front;
+    this.front = frontNode.next;
+    if (this.length === 1) {
+      this.front = null;
+      this.back  = null;
+    }
+    this.length--;
+    return frontNode.value;
+  }
+
+  size(){
+    return this.length;
+  }
+
+  peek(){
+    return this.front.value;
+  }
 }
 
 exports.Node = Node;
